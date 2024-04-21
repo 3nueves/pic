b35af9c62caf1e71ed60d45abc9f325b55812b2b
David Moya <3nueves@gmail.com>
2024-04-21 21:45:57 +0200

    prepare package

---
+++ setup.py
@@ -0,0 +1,13 @@
+""" Module to distribute package """
+from setuptools import setup
+
+setup(
+    name="pic",
+    version="0.1.0",
+    description="App to perform and control tasks on remote and local servers",
+    author="David Moya López",
+    author_email="3nueves@gmail.com",
+    url="https://3ozone.net",
+    packages=[],
+    scripts=[]
+)
