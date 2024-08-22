3f9281bf50f0d7282847cecd17ac37ed07d656d2
David Moya <3nueves@gmail.com>
2024-08-22 21:44:25 +0200

    create testing

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,10 +1,28 @@
 """ Testing """
 import pytest
+import pathlib
+import shutil
 
-from pic.model import CreateFolderAndConfigFile
+from os import path
 
-class TestModel:
+from pic.model.install import Install
+
+# const env
+FOLDER_PIC = '/Users/davidmoyalopez/pic'
+
+class TestInstall:
     """ pytest """
 
-    def test_create_folder():
-        pass
+    def test_install(self):
+        """ Test to check if ok """
+        install = Install()
+        home = pathlib.Path.home()
+        route = home.joinpath('pic')
+        install.create_folders_and_files()
+        shutil.rmtree(route)
+        assert str(route) == FOLDER_PIC
+
+        # self.install.create_kit()
+        # self.install.installed()
+        
+        
