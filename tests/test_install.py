308f2f9371977beda97ea6fc7480502acb42bbf2
David Moya <3nueves@gmail.com>
2024-08-26 21:43:04 +0200

    update testing

--- tests/test_install.py
+++ tests/test_install.py
@@ -8,15 +8,39 @@ from os import path
 from pic.model.install import Install
 
 # const env
-FOLDER_PIC = '/Users/davidmoyalopez/pic'
+FOLDERS = {
+    'pic': 'pic',
+    'kits': 'pic/kits',
+    'kits_sh': 'pic/kits/cmd',
+    'servers': 'pic/servers',
+    'secrets': 'pic/secrets'
+}
+
+FILES = {
+    'pic': 'pic/config.yaml',
+    'pic_install': 'pic/.installed',
+    'kits': 'pic/kits/pic.yaml',
+    'kits_sh': 'pic/kits/cmd/cmd.sh',
+    'kits_pic': 'pic/kits/cmd/pic.yaml',
+    'servers': 'pic/servers/servers.yaml',
+    'secrets': 'pic/secrets/secrets.yaml'
+}
 
 class TestInstall:
     """ pytest """
 
-    def test_install(self):
+    def test_check_class_install(self):
         """ Test to check if ok """
         install = Install()
         home = pathlib.Path.home()
-        route = home.joinpath('pic')
-        shutil.rmtree(route)
-        assert str(route) == FOLDER_PIC
+
+        # Check if exiest Folders
+        for folder in FOLDERS.values():
+            assert path.exists(home.joinpath(folder))
+
+       # Check if exiest Files
+        for file in FILES.values():
+            assert path.exists(home.joinpath(file))
+
+        # Remove folder pic
+        shutil.rmtree(home.joinpath(FOLDERS['pic']))
