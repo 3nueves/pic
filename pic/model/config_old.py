a78d8392c349f4bc0b84e4133ce136769678c8d1
David Moya <3nueves@gmail.com>
2024-08-29 09:25:54 +0200

    update

--- pic/model/config_old.py
+++ pic/model/config_old.py
@@ -6,7 +6,7 @@ import logging
 from envyaml import EnvYAML
 from .create_config_files import CreateFolderAndConfigFile
 
-__version__ = "v0.5.7"
+__version__ = "v0.5.8"
 
 class Config():
     """ Manage path kits """
